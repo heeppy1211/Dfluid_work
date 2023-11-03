a21532f525822b1e39f68607c94d028a6f4b8a59
heeppy1211 <149640062+heeppy1211@users.noreply.github.com>
2023-11-03 20:38:00 +0900

    디플루이드 과제 셋팅
    디플루이드 과제 셋팅

---
+++ workplace/src/layout/Main.js
@@ -0,0 +1,13 @@
+import Reacts from 'react';
+
+function Main() {
+    return (
+        <div className="main">
+            <div className="container">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Main;
